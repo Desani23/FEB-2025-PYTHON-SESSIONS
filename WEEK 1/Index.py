ee03132ea3e0756786daffd675548f3da7244339
Desani23 <ilupejud@yahoo.com>
2025-03-11 09:05:23 +0100

    Introduction to Python

---
+++ WEEK 1/Index.py
@@ -0,0 +1,21 @@
+print("Hello, World!")
+
+number = 10
+print(number)
+
+
+
+
+
+age = 34 #Integer Data Type
+marks = 90.5 #Float Data Type
+name = "John Smith" #String Data Type
+
+
+#List Data Type
+languages = ["Python", "Java", "C++", "C#", 3, 5.9, True] #List Data Type
+print(languages)
+print(languages[2])
+print(languages[5])
+print(languages[-4])
+print(languages[-7])
